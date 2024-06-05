49794f4cb8e746e6300d00ba3b8ba6b9dad87c54
kurisu <kurisu-noreply@amadeuslandingpad.net>
2024-06-05 15:26:30 +0800

    first commit

---
+++ ftpconsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        // FTP 服务器上的中文文件
+        string remoteFile = "ftp://ipaddr/测试文件.txt";
+
+        // 本地保存文件的路径
+        string localFile = "file.txt";
+
+        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(remoteFile);
+        request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+        // 设置 FTP 服务器的用户名和密码
+        request.Credentials = new NetworkCredential("ideleteduser", "notreallyapassword");
+
+        try
+        {
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            using (Stream responseStream = response.GetResponseStream())
+            using (FileStream writer = new FileStream(localFile, FileMode.Create))
+            {
+                responseStream.CopyTo(writer);
+            }
+
+            Console.WriteLine("文件下载成功");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("文件下载失败，错误信息： " + e.Message);
+        }
+    }
+}
